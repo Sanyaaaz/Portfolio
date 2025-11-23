b68948d1e1ab1cc869fa8c145854bef49eac4665
Sanya Saxena <sanyasaxena1465@gmail.com>
2025-11-24 02:37:44 +0530

    Terminal

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import {Navbar,Welcome,Dock} from '#components'
-
+import { Terminal } from '#windows';
+import { Draggable } from 'gsap/Draggable';
+import gsap from "gsap";
+gsap.registerPlugin(Draggable);
 const App = () => {
   return (
     <main> 
       <Navbar/>
       <Welcome/>
       <Dock />
+      <Terminal />
     </main>
   )
 }
